63b2e49708bb616724d346445bea1ab0aa99a75b
Marcus Lo <marcusklo1010@gmail.com>
2024-03-25 21:54:20 -0600

    array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const matches = [];
+  
+    for (let i = 0; i < drivers.length; i++) {
+      const driver = drivers[i];
+      if (driver.toLowerCase() === string.toLowerCase()) {
+        matches.push(driver);
+      }
+    }
+  
+    return matches;
+  }
+  function fuzzyMatch(drivers, query) {
+    const matches = [];
+  
+    for (let i = 0; i < drivers.length; i++) {
+      const driver = drivers[i];
+      if (driver.startsWith(query)) {
+        matches.push(driver);
+      }
+    }
+  
+    return matches;
+  }
+  function matchName(drivers, string) {
+    const matches = [];
+  
+    for (let i = 0; i < drivers.length; i++) {
+      const driver = drivers[i];
+      if (driver.name === string) {
+        matches.push(driver);
+      }
+    }
+  
+    return matches;
+  }
+  
